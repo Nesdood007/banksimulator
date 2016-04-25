31c3bc8d5def68cda2b5d42b95ac8a747dd3e9f8
Amy Vinson <alvsc07@yahoo.com>
2016-04-25 15:48:22 -0400

    fixed errors

--- item.h
+++ item.h
@@ -2,34 +2,35 @@
 //Amy Vinson and Brady O'Leary
 //
 
-#ifndef ITEM_H
-#define ITEM_H
+#ifndef _ITEM_H_
+#define _ITEM_H_
 #include <iostream>
 
 #include <queue>
-#include <vector>
-#include <stdlib.h>
+#include<vector>
+#include<stdlib.h>
 using namespace std;
-
-class item {
+class Item{
+    int key;
     public:
-	item():key(0){}//default item constructor that sets key to zero
-	int getKey(){ 
-		return key;
-	}
-	int setKey(int a) {
-		key = a;
-	}
-	item(int k) : key(k){}
-	friend class compareItem;
-	virtual void Run(){}
-	virtual ~item(){}
+    Item() : key(0) {}
+    int getKey() {return key;}
+    int setKey(int a) {key = a;}
+    Item(int k) : key (k) {}
+    friend class compareItem;
+    virtual void Run() {}
+    virtual ~Item(){}
 };
 
-struct compareItem : binary_function<Item*, Item*, bool> {
-    bool operator()(const Item* t1, const Item* t2) const {
+struct compareItem
+: binary_function<Item*, Item*, bool>
+{
+    bool operator()(const Item* t1, const Item* t2) const
+    {
         return (t1->key > t2->key);
     }
-}
+};
 
+ 
 #endif
+
